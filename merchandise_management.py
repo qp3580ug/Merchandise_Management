5708ee9b7c288dbf83f7226ac1edff8df79ae8f0
Nathan W <nathan@email.com>
2019-02-26 19:36:19 -0600

    Added function to add new merchandise to table

--- merchandise_management.py
+++ merchandise_management.py
@@ -1,6 +1,9 @@
 import sqlite3
+import os
+import sys
 
 num = 0
+
 
 def main():
     db = sqlite3.connect('critical_roll_tour_merch.db')
@@ -13,26 +16,39 @@ def main():
     num = int(input('Enter number here: '))
 
     if num == 1:
-        add_new_merchandise()
+        add_new_merchandise(num, cur)
     if num == 2:
-        update_merchandiseInventory()
+        update_merchandiseInventory(num)
     if num == 3:
-        add_new_tour_date_town()
+        add_new_tour_date_town(num)
     if num == 4:
-        add_new_receipt()
+        add_new_receipt(num)
     if num == 5:
-        show_inventory()
+        show_inventory(num, cur)
 
 
-def add_new_merchandise():
+def add_new_merchandise(num, cur):
+    merchName = input('Enter type of Merchandise to add: ')
+    merchAmount = input('Enter total number of Merchandise in stock: ')
+    cur.execute('insert into merchandiseInventory values (?, ?)', (merchName, merchAmount))
+    os.execl(sys.executable, sys.executable, *sys.argv)
 
-def update_merchandiseInventory():
-    
-def add_new_tour_date_town():
+def update_merchandiseInventory(num):
+    num = 0
+    return num
 
-def add_new_receipt():
+def add_new_tour_date_town(num):
+    num = 0
+    return num
 
-def show_inventory():
+def add_new_receipt(num):
+    num = 0
+    return num
 
+def show_inventory(num, cur):
+    for row in cur.execute('select * from merchandiseInventory'):
+        print(row)
+    num = 0
+    return num
 
 main()
