5b4ce8ac7fe1f879e4aed213c113aa896c4ac4ad
Nathan W <nathan@email.com>
2019-02-26 19:54:35 -0600

    Added functions to add to other tables, update merchandise inventory, show inventory

--- merchandise_management.py
+++ merchandise_management.py
@@ -16,39 +16,45 @@ def main():
     num = int(input('Enter number here: '))
 
     if num == 1:
-        add_new_merchandise(num, cur)
+        add_new_merchandise(cur)
     if num == 2:
-        update_merchandiseInventory(num)
+        update_merchandiseInventory(cur)
     if num == 3:
-        add_new_tour_date_town(num)
+        add_new_tour_date_town(cur)
     if num == 4:
-        add_new_receipt(num)
+        add_new_receipt(cur)
     if num == 5:
-        show_inventory(num, cur)
+        show_inventory(cur)
 
 
-def add_new_merchandise(num, cur):
+def add_new_merchandise(cur):
     merchName = input('Enter type of Merchandise to add: ')
     merchAmount = input('Enter total number of Merchandise in stock: ')
     cur.execute('insert into merchandiseInventory values (?, ?)', (merchName, merchAmount))
     os.execl(sys.executable, sys.executable, *sys.argv)
 
-def update_merchandiseInventory(num):
-    num = 0
-    return num
+def update_merchandiseInventory(cur):
+    merchName = input('Enter merchandise you would like to change: ')
+    merchAmount = input('Enter new total of merchandise: ')
+    cur.execute('update merchandiseInventory where merchName=? set merchAmount=?', (merchName, merchAmount))
+    os.execl(sys.executable, sys.executable, *sys.argv)
 
-def add_new_tour_date_town(num):
-    num = 0
-    return num
+def add_new_tour_date_town(cur):
+    townName = input('Please add a town name for the tour: ')
+    dateVisited = input('Please add the date this town was visited: ')
+    cur.execute('insert into tourLocations values (?, ?)', (townName, dateVisited))
+    os.execl(sys.executable, sys.executable, *sys.argv)
 
-def add_new_receipt(num):
-    num = 0
-    return num
+def add_new_receipt(cur):
+    merchName = input('Please enter merchandise on receipt: ')
+    townName = input('Please enter the receipt town name where purchased: ')
+    merchSold = int(input('Please enter total of this merchandise sold: '))
+    cur.execute('insert into merchPurchased values (?, ?, ?)', (merchName, townName, merchSold))
+    os.execl(sys.executable, sys.executable, *sys.argv)
 
-def show_inventory(num, cur):
+def show_inventory(cur):
     for row in cur.execute('select * from merchandiseInventory'):
         print(row)
-    num = 0
-    return num
+    os.execl(sys.executable, sys.executable, *sys.argv)
 
 main()
